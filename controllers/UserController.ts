77255d23539e389e8ac46bd43ced0a41e2da9668
fadilkun45 <indocrafter76@gmail.com>
2024-05-14 17:00:50 +0700

    token

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -4,19 +4,27 @@ import jwt from 'jsonwebtoken'
 
 export class UserController {
 
-    static login(req: Request, res:Response)  {
+    static async login(req: Request, res:Response)  {
 
         console.log(req.body.userToken)
 
-        const decryptToken = crypto.createDecipher('aes-256-cbc', process.env.KEY_USER!)
-        let decryptedId = decryptToken.update(req.body.userToken, 'hex', 'utf8');
-        decryptedId += decryptToken.final('utf8');
+        try {
+            const decryptToken = crypto.createDecipher('aes-256-cbc', process.env.KEY_USER!)
+            let decryptedId = decryptToken.update(req.body.userToken, 'hex', 'utf8');
+            decryptedId += decryptToken.final('utf8');
+    
+            const token = jwt.sign({ id: decryptedId }, process.env.KEY_JWT_USER!);
+             
+            res.status(200).json({
+                message: "token berhasil dibuat",
+                userToken: token 
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: "token tidak valid",
+            })
+        }
 
-        const token = jwt.sign({ id: decryptedId }, process.env.KEY_JWT_USER!);
-         
-        res.status(200).json({
-            message: "token berhasil dibuat",
-            userToken: token 
-        })
+       
     }
 }
